d81d69be9873c6507cfef1178d9292d482317224
Javier Sánchez Fernández <sanchezfdez.javier@gmail.com>
2020-12-22 15:39:11 +0100

    Submit on enter done

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { QUIZZES_URL } from './constants';
 import { FaCheck } from "react-icons/fa";
 class App extends Component {
   componentDidMount(){
+    console.log("triggered")
     this.props.dispatch(initQuizzes())
   }
 
@@ -17,7 +18,7 @@ class App extends Component {
     console.log("event triggered")
     if(e.keyCode == 13 && e.shiftKey == false) {
       e.preventDefault();
-      this.myFormRef.submit();
+      this.props.dispatch(submit(this.props.quizzes))
     }
   }
 
@@ -26,30 +27,31 @@ class App extends Component {
     if(this.props.quizzes.length === 0){
       return (
         <div className="App">
-          <header className="App-header slide-in-top" key={ Math.floor(Math.random() * 10) }>
-              <Navbar/>
+          <div className="jumbotron">
+            <header className="App-header slide-in-top" key={ Math.floor(Math.random() * 10) }>
+                <Navbar/>
             </header>
-          <div className="container justify-content-center">
-            <div className="row">
-            <h1 className="text-center display-2 m-4"> No quizzes load </h1>
-            <button className="btn btn-lg btn-primary text-center" 
-                    onClick={()=>{ this.props.dispatch(initQuizzes(QUIZZES_URL))}}>
-                    Go to the first page!
-            </button>
+            <div className="container justify-content-center align-content-center">
+              
+              <h1 className="text-center display-4 m-4"> No quizzes load </h1>
+              <button className="btn btn-lg btn-primary text-center" 
+                      onClick={()=>{ this.props.dispatch(initQuizzes(QUIZZES_URL))}}>
+                      Go to the first page!
+              </button>            
             </div>
           </div>
         </div>
       )
     } else {
       return (
-        <div className="App">
+        <div className="App" onKeyDown={ this.onEnterPress }>
           <header className="App-header slide-in-top">
             <Navbar/>
           </header>
     
-          <div className="container mt-4">
+          <div className="container-fluid mt-4">
             <div className="row">
-              <div className="col-md-5">
+              <div className="col-md-5 slide-in-left">
                   <List quizzes={ this.props.quizzes } 
                         currentQuiz={ this.props.currentQuiz } 
                         onChangeQuiz={(i) => {
@@ -58,21 +60,23 @@ class App extends Component {
                         />
                   <div className="row">
                         <div className="col-8 text-start m-2">
-                          <div className="rounded-lg" onKeyDown={ this.onEnterPress }>
+                          <div className="rounded-lg">
                               <button type="submit"
-                                      className="btn btn-lg btn-success shadow-sm" 
+                                      className="btn btn-lg btn-success shadow-sm m-1" 
                                       onClick={ ()=>{ this.props.dispatch(submit(this.props.quizzes))} }
                                       >
                                       Submit
                               </button>
-                              <button className="btn btn-lg btn-primary ms-3 shadow-sm" 
+                              <button className="btn btn-lg btn-primary m-1 shadow-sm" 
                                       onClick={()=>{ this.props.dispatch(initQuizzes(QUIZZES_URL))}}>
                                       Load more quizzes
                               </button>
                           </div>
                         </div>
-                        <div className="col text-center m-1">
-                          <p className="score display-6"><FaCheck/> {this.props.score}</p>
+                        <div className="col m-1 score text-start align-content-center">
+                          <div className="">
+                              <FaCheck width="5"/> {this.props.score}
+                          </div>
                         </div>
                   </div>
               </div>
